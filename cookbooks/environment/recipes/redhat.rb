13ec2c788dd62186c1f8cbec53d4c29f7a4436a0
Luan Guimarães <livreluan@gmail.com>
2017-06-19 21:28:12 -0300

    Adding gcc-c++ package and removing vagrant files

--- cookbooks/environment/recipes/redhat.rb
+++ cookbooks/environment/recipes/redhat.rb
@@ -5,7 +5,7 @@ execute 'yum update -y'
 yum_package 'epel-release'
 execute 'yum update'
 
-%w( make cmake SDL2-devel SDL2_image-devel
+%w( gcc-c++ make cmake SDL2-devel SDL2_image-devel
     SDL2_ttf-devel SDL2_mixer-devel).each do |p|
   package p
 end
